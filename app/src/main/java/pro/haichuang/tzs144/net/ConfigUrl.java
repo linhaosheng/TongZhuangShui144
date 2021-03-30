c3f1ce950cd3477267c2241afdc0ac8dc97ea6a9
linhaoshneg <lin12826345972163.com>
2021-03-30 17:15:46 +0800

    修改UI显示不全

--- app/src/main/java/pro/haichuang/tzs144/net/ConfigUrl.java
+++ app/src/main/java/pro/haichuang/tzs144/net/ConfigUrl.java
@@ -14,7 +14,7 @@ public class ConfigUrl {
      * base url
      */
     @DefaultDomain
-    public static final String BASE_URL = "http://114.215.137.110:9090";  // https://api.ssssedc.com/  http://114.215.137.110:9090   https://api-tzs144.haichuang.pro
+    public static final String BASE_URL = "https://api-tzs144.haichuang.pro";  // https://api.ssssedc.com/  http://114.215.137.110:9090   https://api-tzs144.haichuang.pro
 
     /**
      * 登录
