f7862d03ec00858c7710391b6501fe53e41415a8
linhaoshneg <lin12826345972163.com>
2021-04-11 11:41:24 +0800

    修改ip地址

--- app/src/main/java/pro/haichuang/tzs144/net/ConfigUrl.java
+++ app/src/main/java/pro/haichuang/tzs144/net/ConfigUrl.java
@@ -14,7 +14,7 @@ public class ConfigUrl {
      * base url
      */
     @DefaultDomain
-    public static final String BASE_URL = "http://114.215.137.110:9090";  // https://api.ssssedc.com/   https://api-tzs144.haichuang.pro  http://114.215.137.110:9090
+    public static final String BASE_URL = "https://api-tzs144.haichuang.pro";  // https://api.ssssedc.com/   https://api-tzs144.haichuang.pro  http://114.215.137.110:9090
 
     /**
      * 登录
